a238f90d3969ad80c214f48e4461daf87094d156
Tomoya Hagen <tomoyahagen@Tomoyas-MBP.telekom.ip>
2024-11-23 19:30:39 +0100

    add tests

--- tests/rstest.cpp
+++ tests/rstest.cpp
@@ -4,7 +4,10 @@
  * See LICENSE */
 
 #include "rstest.hpp"
+#include <iomanip>
 #include <rs.hpp>
+#include <vector>
+#include <random>
 
 Report
 RStest::run_tests() {
@@ -21,6 +24,48 @@ RStest::run_tests() {
     }
 
     std::cout << "RSTest: " << rep.passed << "/" << rep.overall << " tests passed.\n\n";
+
+    if (!RStest::test_encode_decode()) {
+        std::cout << "Encode-decode test failed!\n" << std::endl;
+    } else {
+        std::cout << "Encode-decode test passed!\n" << std::endl;
+    }
+
+    if (!RStest::test_encode_decode_2()) {
+        std::cout << "Encode-decode test 2 failed!\n" << std::endl;
+    } else {
+        std::cout << "Encode-decode test 2 passed!\n" << std::endl;
+    }
+
+    if (!RStest::test_single_error()) {
+        std::cout << "Single error test failed!\n" << std::endl;
+    } else {
+        std::cout << "Single error test passed!\n" << std::endl;
+    }
+
+    if (!RStest::test_half_parity_corrupted()) {
+        std::cout << "Half parity corrupted test failed!\n" << std::endl;
+    } else {
+        std::cout << "Half parity corrupted test passed!\n" << std::endl;
+    }
+
+    if (!RStest::test_really_long_data()) {
+        std::cout << "Really long data test failed!\n" << std::endl;
+    } else {
+        std::cout << "Really long data test passed!\n" << std::endl;
+    }
+
+    if (!RStest::test_really_short_data()) {
+        std::cout << "Really short data test failed!\n" << std::endl;
+    } else {
+        std::cout << "Really short data test passed!\n" << std::endl;
+    }
+
+    // if (!RStest::stress_test((size_t)128, 1000, 8)) {
+    //     std::cout << "Stress test failed!\n" << std::endl;
+    // } else {
+    //     std::cout << "Stress test passed!\n" << std::endl;
+    // }
 
     return rep;
 }
@@ -76,7 +121,226 @@ RStest::test_decode(std::string &name) {
     RETURN
 }
 
-// TODO make complex encoder-decder test
+bool RStest::test_encode_decode() {
+    char message[] = "Some very important message ought to be delivered";
+    const int msglen = sizeof(message);
+    const int ecclen = 8;
+
+    char repaired[msglen];
+    char encoded[msglen + ecclen];
+
+
+    RS::ReedSolomon<msglen, ecclen> rs;
+
+    rs.Encode(message, encoded);
+
+    // Corrupting first 8 bytes of message (any 4 bytes can be repaired, no more)
+    for(uint i = 0; i < ecclen / 2; i++) {
+        encoded[i] = 'E';
+    }
+
+    rs.Decode(encoded, repaired);
+
+    std::cout << "Original:  " << message  << std::endl;
+    std::cout << "Corrupted: " << encoded  << std::endl;
+    std::cout << "Repaired:  " << repaired << std::endl;
+
+    return (memcmp(message, repaired, msglen) == 0);
+}
+
+bool
+RStest::test_encode_decode_2() {
+    uint8_t message[] = "102042018744466623571113171923";
+    uint8_t encoded[sizeof(message) + 8];
+    uint8_t buffer_std[sizeof(message)];
+    uint8_t buffer_crp[sizeof(message)];
+
+    RS::ReedSolomon<sizeof(message), 8> rs;
+
+    rs.Encode(message, encoded);
+
+    uint8_t corrupted[sizeof(encoded)] = {0};
+
+    for (int i = 0; i < sizeof(encoded); i++) {
+        corrupted[i] = encoded[i];
+    }
+
+    corrupted[5] = 9;
+    corrupted[6] = 9;
+    corrupted[7] = 9;
+    corrupted[8] = 9;
+
+    rs.Decode(encoded, buffer_std);
+    rs.Decode(corrupted, buffer_crp);
+
+    auto print_hex = [](const uint8_t* data, size_t length) {
+        for (size_t i = 0; i < length; ++i) {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(data[i]);
+        }
+        std::cout << std::dec << std::endl; // Reset to decimal
+    };
+
+    std::cout << "Original:  ";
+    print_hex(message, sizeof(message));
+    std::cout << "Encoded:   ";
+    print_hex(encoded, sizeof(encoded));
+    std::cout << "Corrupted: ";
+    print_hex(corrupted, sizeof(corrupted));
+    std::cout << "Repaired std:  ";
+    print_hex(buffer_std, sizeof(buffer_std));
+    std::cout << "Repaired crp:  ";
+    print_hex(buffer_crp, sizeof(buffer_crp));
+
+    return (memcmp(message, buffer_std, sizeof(message)) == 0 && memcmp(message, buffer_crp, sizeof(message)) == 0);
+}
+
+bool RStest::test_single_error() {
+    uint8_t message[] = "102042018744466623571113171923";
+    uint8_t encoded[sizeof(message) + 8];
+    uint8_t buffer_std[sizeof(message)];
+    uint8_t buffer_crp[sizeof(message)];
+
+    RS::ReedSolomon<sizeof(message), 8> rs;
+    rs.Encode(message, encoded);
+
+    uint8_t corrupted[sizeof(encoded)];
+    memcpy(corrupted, encoded, sizeof(encoded));
+    corrupted[0] ^= 0xFF;  // Introduce a single error
+
+    rs.Decode(encoded, buffer_std);
+    rs.Decode(corrupted, buffer_crp);
+
+    return memcmp(message, buffer_std, sizeof(message)) == 0 &&
+           memcmp(message, buffer_crp, sizeof(message)) == 0;
+}
+
+// adding one more corruption makes it fail, meaning 8 Bytes are the max. as expected!
+bool RStest::test_half_parity_corrupted() {
+    uint8_t message[] = "102042018744466623571113171923";
+    uint8_t encoded[sizeof(message) + 8];
+    uint8_t buffer_std[sizeof(message)];
+    uint8_t buffer_crp[sizeof(message)];
+
+    RS::ReedSolomon<sizeof(message), 8> rs;
+    rs.Encode(message, encoded);
+
+    uint8_t corrupted[sizeof(encoded)];
+    memcpy(corrupted, encoded, sizeof(encoded));
+    for (int i = sizeof(message); i < sizeof(encoded); i += 2) {
+        corrupted[i] ^= 0xFF;  // Corrupt every other parity symbol
+    }
+
+    rs.Decode(encoded, buffer_std);
+    rs.Decode(corrupted, buffer_crp);
+
+    return memcmp(message, buffer_std, sizeof(message)) == 0 &&
+           memcmp(message, buffer_crp, sizeof(message)) == 0;
+}
+
+void generate_random_message(uint8_t *message, size_t length) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
+
+    for (size_t i = 0; i < length; ++i) {
+        message[i] = dis(gen);
+    }
+}
+
+void introduce_errors(uint8_t *encoded, size_t length, int num_errors) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> pos_dis(0, length - 1);
+    std::uniform_int_distribution<> val_dis(0, 255);
+
+    for (int i = 0; i < num_errors; ++i) {
+        size_t pos = pos_dis(gen);
+        uint8_t error_value = val_dis(gen);
+        encoded[pos] ^= error_value; // Introduce error
+    }
+}
+
+bool stress_test(size_t message_length, int num_tests, int max_errors) {
+    std::array<uint8_t, 128> message;
+    std::vector<uint8_t> encoded(message_length + 8);
+    std::vector<uint8_t> decoded(message_length);
+    std::vector<uint8_t> corrupted(message_length + 8);
+
+    const uint8_t msg_len = message_length;
+
+    RS::ReedSolomon<128, 8> rs;
+
+    int success_count = 0;
+
+    for (int i = 0; i < num_tests; ++i) {
+        generate_random_message(message.data(), message_length);
+        rs.Encode(message.data(), encoded.data());
+
+        // Test with different number of errors
+        for (int errors = 0; errors <= max_errors; ++errors) {
+            std::copy(encoded.begin(), encoded.end(), corrupted.begin());
+            introduce_errors(corrupted.data(), corrupted.size(), errors);
+
+            rs.Decode(corrupted.data(), decoded.data());
+
+            if (memcmp(message.data(), decoded.data(), message_length) == 0) {
+                success_count++;
+            }
+        }
+    }
+
+    std::cout << "Total tests: " << num_tests * (max_errors + 1) << std::endl;
+    std::cout << "Successful decodings: " << success_count << std::endl;
+    std::cout << "Success rate: " << (double)success_count / (num_tests * (max_errors + 1)) * 100 << "%" << std::endl;
+
+    return true;
+}
+
+bool RStest::test_really_short_data() {
+    char msg[] = "Hello";
+    char encoded[sizeof(msg) + 2];
+    char buffer_std[sizeof(msg)];
+
+    RS::ReedSolomon<sizeof(msg), 2> rs;
+
+    rs.Encode(msg, encoded);
+
+    for (char c : msg) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c);
+    }
+    std::cout << std::endl;
+
+    for (char c : encoded) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c);
+    }
+    std::cout << std::endl;
+
+    encoded[5] = 'a';
+
+    rs.Decode(encoded, buffer_std);
+
+    return memcmp(msg, buffer_std, sizeof(msg)) == 0;
+}
+
+// currently: one error and 2 parity bits -> passes
+bool RStest::test_really_long_data() {
+    char msg[240];
+    for (int i = 0; i < 240; i++) {
+        msg[i] = i - '0';
+    }
+
+    char endcoded[sizeof(msg) + 1];
+    char buffer_std[sizeof(msg)];
+
+    RS::ReedSolomon<sizeof(msg), 2> rs;
+
+    rs.Encode(msg, endcoded);
+    endcoded[5] = 'a';
+    rs.Decode(endcoded, buffer_std);
+
+    return memcmp(msg, buffer_std, sizeof(msg)) == 0;
+
+}
 
 bool
 RStest::test_stress(std::string &name) {
--- tests/testsuite.hpp
+++ tests/testsuite.hpp
@@ -1,1 +1,118 @@
-mtest/include/testsuite.hpp
+#ifndef TESTSUITE_H
+#define TESTSUITE_H
+#include <iostream>
+#include <string.h>
+
+struct Report {
+    uint32_t overall;
+    uint32_t passed;
+};
+
+struct PReport {
+    uint32_t laps[64];
+    uint32_t overall;
+    uint32_t average[64];
+    uint32_t count;
+    uint32_t passed;
+};
+
+class TestSuite {
+protected:
+    template<typename T>
+    static bool compare(const T var, const T ans) {
+        if(var != ans) {
+            std::cout << "expected: " << ans << std::endl;
+            std::cout << "got:      " << var << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    static bool compare(const char* var, const char* ans, bool compare_len) {
+        size_t varlen = strlen(var);
+        size_t anslen = strlen(ans);
+
+        if(varlen != anslen && compare_len) {
+            std::cout << "expected length " << anslen << ", "
+                      << "got " << varlen << std::endl;
+            return false;
+        }
+
+        size_t cnt = (compare_len) ? strlen(var) : strlen(ans);
+
+        if(strncmp(var, ans, cnt) != 0) {
+            std::cout << "expected: " << ans << std::endl;
+            std::cout << "got:      " << var << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool compare(const void *vvar, const void *vans, size_t varlen, size_t anslen = 0) {
+        char* var = (char*) vvar;
+        char* ans = (char*) vans;
+        if(!anslen) anslen = varlen;
+        if(varlen != anslen) {
+            std::cout << "expected length " << anslen << ", "
+                      << "got " << varlen << std::endl;
+            return false;
+        }
+
+        if(memcmp(var, ans, varlen) != 0) {
+            std::cout << "expected: "; print_array(ans, anslen);
+            std::cout << "got:      "; print_array(var, varlen);
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool test(bool (*fptr)(std::string&)) {
+        std::string name;
+        bool ok = fptr(name);
+        std::cout << name << ": " << (ok ? "SUCCESS" : "FAILURE") << std::endl;
+        return ok;
+    }
+
+    // Find substring
+    static bool has_substr(void* buffer, size_t bufsize, void* substr, size_t substrsize) {
+    #ifdef __GNUG__ // If GNU extensions are present, use memmem
+        return (memmem(buffer, bufsize, substr, substrsize) != NULL);
+    #else           // Else do dirty trick with strings
+        std::string haystick((char*)buffer, (char*)buffer+bufsize);
+        std::string needle((char*)substr, (char*)substr+substrsize);
+        return haystick.find(needle) != std::string::npos;
+    #endif
+    }
+
+    #define INIT_TESTCASE               \
+            name = __FUNCTION__;  \
+            int  _stnum = 0;              \
+            bool _ok = true;               \
+            bool _temp;
+
+    #define SUBTEST(expr) \
+            _stnum++;      \
+            _temp = (expr); \
+            _ok &= _temp;    \
+            if(!_temp) {      \
+                std::cout << __FUNCTION__ << ":"                      \
+                          << "SubTest #" << _stnum << " \"" #expr "\"" \
+                          << " failed.\n";                              \
+            }
+
+    #define RETURN \
+            return _ok;
+
+private:
+    static void print_array(char *array, uint length) {
+        std::cout << std::hex;
+        for(uint i = 0; i < length; i++) {
+            std::cout << (int)array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+#endif // TESTSUITE_H
