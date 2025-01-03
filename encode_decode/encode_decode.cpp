1211669120f37439da4e106928fc85db35c63c60
Tomoya Hagen <tomoya.hagen7@gmail.com>
2025-01-03 02:03:44 +0100

    fix errors, Encode refinement WIP

--- encode_decode/encode_decode.cpp
+++ encode_decode/encode_decode.cpp
@@ -13,16 +13,39 @@ namespace sts1cobcsw
 
     void Encode(std::span<std::uint8_t> src, std::vector<std::uint8_t> &dst)
     {
-        auto encoded = std::vector<std::uint8_t>();
-        rs.Encode(src, encoded);
-        bitsn::scramble(encoded);
+        int input_size = src.size();
+        int block_size = 223;
+        int ecc_length = 32;
+        const int total_blocks = (input_size + block_size - 1) / block_size;
+        uint8_t encoded[total_blocks][block_size + ecc_length];
+
+        for (int i = 0; i < total_blocks; i++)
+        {
+            int start_idx = i * block_size;
+            int current_block_size = std::min(block_size, total_message_length - start_idx);
+            uint8_t block[block_size] = {0};
+            memcpy(block, message + start_idx, current_block_size);
+
+            rs.Encode(block, encoded[i]);
+        }
+
+        uint8_t code[total_blocks * (block_size + ecc_length)] = {0}; // ignores the filling zeroes!
+        for (int i = 0; i < total_blocks; i++)
+        {
+            memcpy(code + (i * (block_size + ecc_length)), encoded[i], block_size + ecc_length);
+            // bitsn::scramble(encoded[i]); // probably the order of process wrong, since encoded block-wise. Maybe a new array
+            // of length of the whole encoded RS-Code.
+        }
+        bitsn::scramble(code);
+
         auto codec = ViterbiCodec();
-        codec.Encode(encoded, dst);
+        codec.Encode(code, dst);
     }
     void Decode(std::span<std::uint8_t> src, std::vector<std::uint8_t> &dst)
     {
-        bitsn::un_scramble(src);
-        rs.Decode(src, dst);
+        auto buffer = std::vector<std::uint8_t>(src.begin(), src.end());
+        bitsn::un_scramble(buffer);
+        rs.Decode(buffer.data(), dst.data());
     }
 
 }
--- encode_decode/test.cpp
+++ encode_decode/test.cpp
@@ -36,18 +36,93 @@ void test_encode_decode_short() {
         buffer.push_back(bit - '0');
     }
 
+    auto destination = std::vector<std::uint8_t>();
+    sts1cobcsw::Decode(buffer, destination);
+
+    assert(src.size() == destination.size());
     for (int i = 0; i < src.size(); i++) {
-        assert(src[i] == buffer[i]);
+        assert(src[i] == destination[i]);
     }
     std::cout << "Test 'test_encode_decode_short' passed" << "\n";
 }
 
 void test_encode_decode_long() {
+    auto src = std::vector<std::uint8_t>();
+
+    for (int i = 0; i < 1000; i++) {
+        src.push_back(rand() % 255);
+    }
+
+    std::vector<std::uint8_t> dst;
+    sts1cobcsw::Encode(src, dst);
+
+    auto encoded = std::string();
+
+    for (auto x : dst) {
+        encoded += (x - '0');
+    }
+
+    auto decoded = codec.DecodeToString(encoded);
+
+    std::vector<std::uint8_t> buffer;
+
+    for (char bit : decoded) {
+        buffer.push_back(bit - '0');
+    }
+
+    auto destination = std::vector<std::uint8_t>();
+    sts1cobcsw::Decode(buffer, destination);
+
+    assert(src.size() == destination.size());
+    for (int i = 0; i < src.size(); i++) {
+        assert(src[i] == destination[i]);
+    }
+    std::cout << "Test 'test_encode_decode_long' passed" << "\n";
 
 }
 
 void test_encode_decode_insert_error_success() {
+    auto src = std::vector<std::uint8_t>();
 
+    for (int i = 0; i < 500; i++) {
+        src.push_back(rand() % 255);
+    }
+
+    std::vector<std::uint8_t> dst;
+    sts1cobcsw::Encode(src, dst);
+
+    auto encoded = std::string();
+
+    for (auto x : dst) {
+        encoded += (x - '0');
+    }
+
+    int errors = 8;
+    assert(errors < encoded.size());
+    int pos = encoded.size() / 2;
+    for (int i = 0; i < errors; i++) {
+        if (encoded[pos + i] == '0') {
+            encoded[pos + i] = '1';
+        } else {
+            encoded[pos + i] = '0';
+        }
+    }
+    auto decoded = codec.DecodeToString(encoded);
+
+    auto buffer = std::vector<std::uint8_t>();
+
+    for (char bit : decoded) {
+        buffer.push_back(bit - '0');
+    }
+
+    auto destination = std::vector<std::uint8_t>();
+    sts1cobcsw::Decode(buffer, destination);
+
+    assert(src.size() == destination.size());
+    for (int i = 0; i < src.size(); i++) {
+        assert(src[i] == destination[i]);
+    }
+    std::cout << "Test 'test_encode_decode_insert_error_success' passed" << "\n";
 }
 
 void test_encode_decode_insert_error_fail() {
