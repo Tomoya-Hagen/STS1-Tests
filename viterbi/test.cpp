5fc64b09e64bf4862547bc2a0091ecaf850c0b3d
Tomoya Hagen <tomoya.hagen7@gmail.com>
2025-01-20 14:34:00 +0100

    Rewrite test + some details

--- viterbi/test.cpp
+++ viterbi/test.cpp
@@ -11,26 +11,31 @@
 
 int main(int argc, char** argv) {
   auto codec = ViterbiCodec();
-  auto message = std::vector<std::uint8_t>{0b10000001, 1, 3, 4, 2, 1, 3, 5, 10, 11, 2, 2, 2, 2, 2, 2, 1};
+  auto message = std::vector<std::uint8_t>{0b11111111};
   for (std::uint8_t x : message) {
     std::cout << std::format("{:08b}", x) << " ";
   }
   std::cout << "\n" << "size: " << message.size() << "\n";
   auto bits = std::array<std::uint8_t, 48>{};
-
   auto encoded = std::vector<std::uint8_t>();
   codec.Encode(message, encoded);
   auto encoded_string = std::string();
   auto counter = 0;
   for (auto x : encoded) {
-    encoded_string += std::to_string(x) + " ";
+    auto buffer = std::string();
+    while (x) {
+      buffer += std::to_string(x & 1);
+      x >>= 1;
+    }
+    std::reverse(buffer.begin(), buffer.end());
+    encoded_string += buffer;
     counter++;
   }
   std::cout << encoded_string << "\n" << "size: " << counter << "\n";
 
-  auto decoded = codec.Decode(encoded_string);
-  std::cout << "decoded: ";
-  std::cout << decoded << "\n";
+  // auto decoded = codec.Decode(encoded_string);
+  // std::cout << "decoded: ";
+  // std::cout << decoded << "\n";
 
   return 0;
 }
--- viterbi/viterbi.cpp
+++ viterbi/viterbi.cpp
@@ -93,7 +93,7 @@ void ViterbiCodec::Encode(std::span<std::uint8_t> src, std::vector<std::uint8_t>
   for (; i < src.size(); i++)
   {
     auto counter = 0;
-    for (int j = CHAR_BIT - 1; j >= 0; j = j - 2) // per iteration 12 bits.
+    for (int j = CHAR_BIT - 1; j > 0; j = j - 2) // per iteration 12 bits.
     {
       auto bit1 = (src[i] >> j) & 1;
       auto bit2 = (src[i] >> (j - 1)) & 1;
@@ -105,20 +105,17 @@ void ViterbiCodec::Encode(std::span<std::uint8_t> src, std::vector<std::uint8_t>
       state = NextState(state, bit2);
       std::uint8_t output = (output1 << 1) | (output2 & 1);
       assert(output >= 0 && output <= 0b111);
-      // std::cout << std::format("{:04b} ", output);
       bytes = (bytes << 3) | output;
     }
-    std::cout << "\n";
-    if (i & 1) // when i is odd, the total bits are 24, thus 3 bytes.
+
+    if (i % 2 == 1) // when i is odd, the total bits are 24, thus 3 bytes.
     {
       assert(bytes >= 0 && bytes <= 0xFFFFFF);
       int mask = 0xFF0000;
       int shift = 16;
-      std::cout << "bytes: " << std::format("{:024b}", bytes) << std::endl;
       for (int k = 0; k < 3; k++)
       {
         dst.push_back((bytes & mask) >> shift);
-        std::cout << std::format("{:08b} ", (bytes & mask) >> shift) << "\n";
         mask >>= 8;
         shift -= 8;
       }
@@ -126,30 +123,11 @@ void ViterbiCodec::Encode(std::span<std::uint8_t> src, std::vector<std::uint8_t>
     }
   }
 
-  if (i & 1) { // think about how to format the last 4 out of remaining 12 bits correctly. TODO
+
+  if (i % 2 == 1) {
     assert(bytes >= 0 && bytes <= 0xFFF);
     dst.push_back((bytes & 0xFF0) >> 8);
     dst.push_back(bytes & 0xF);
-  }
-
-  auto bits_for_flush = 0;
-  // Encode (constaint_ - 1) flushing bits.
-  for (int i = 0; i < constraint_ - 1; i = i + 2) // 9 more bits here.
-  {
-    assert(constraint_ == 7);
-    auto output1 = Output(state, 0);
-    state = NextState(state, 0);
-    auto output2 = Output(state, 0);
-    state = NextState(state, 0);
-    std::uint8_t output = (output1 << 1) | (output2 & 1);
-    bits_for_flush = (bits_for_flush << 3) | output;
-    if (i == constraint_ - 3) { // hard coded.
-      std::cout << "bytes from flushing: " << std::format("{:09b}", bits_for_flush) << "\n";
-      std::uint8_t second_byte = bits_for_flush & 1;
-      bits_for_flush >>= 1;
-      dst.push_back(bits_for_flush);
-      dst.push_back(second_byte); // adding 1 xor 0.
-    }
   }
 }
 
